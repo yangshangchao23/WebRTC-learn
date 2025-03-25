0fb07ec192bb8d9dc20f046227f08d88463b5de7
shawnyoung <990869833@qq.com>
2025-03-26 07:05:11 +0800

    ssh push

--- WebRTC-clone/webrtc-client/src/components/HelloWorld.vue
+++ WebRTC-clone/webrtc-client/src/components/HelloWorld.vue
@@ -7,7 +7,7 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1 class="m-10">{{ msg }}</h1>
+  <h1>{{ msg }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
