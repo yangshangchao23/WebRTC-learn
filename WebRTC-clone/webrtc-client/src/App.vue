1e6e534bb1382ca2d00b62f8ba8d82663ee1cbb0
shawnyoung <990869833@qq.com>
2025-04-01 01:23:16 +0800

    完成

--- WebRTC-clone/webrtc-client/src/App.vue
+++ WebRTC-clone/webrtc-client/src/App.vue
@@ -18,9 +18,22 @@
 //   5-4.B监听A发送offer动作，创建自己的PeerConnection对象，本地流渲染、创建DSP信息（设置远端描述的offer、生成answer、创建本地描述的answer等）。最后发送一个answer给信令服务器
 //   5-5.A监听B发送answer动作（信令服务器新建一个receiveAnswer动作），设置远端描述的answer
 
-// 至此，媒体协商动作完成  调用了API createOffer + setLocalDescription + createAnswer + setRemoteDescription
+// 至此，媒体协商动作完成SDP信息交换  目的-双边编解码数据？ 调用了API createOffer + setLocalDescription + createAnswer + setRemoteDescription
 
-// 开始交换网络信息candidate
+// 开始交换网络信息candidate  目的？
+// 6.A获取本地candidate信息并且通过信令服务器发送B
+//  6-1.时机：监听到有人接受通话后，发送offer动作时。
+// 7.B监听接收candidate信息，添加candiate信息 -addIceCandidate
+// 8.B获取本地candidate信息，通过信令服务器发送给B
+//   8-1.时机：监听到B接受通话后，发送answer动作时。
+// 9.A添加B的candidate信息。代码同7，不用重复写
+
+// 至此，candidate交换完成。开始P2P连接
+
+// 10.接下来用户A和用户B就可以进行P2P通信流
+// 10-1. 拿到对方的流-onaddstream API，渲染到自己的页面。
+//    时机:A收到B触发的接收时，即'acceptCall'。B收到A发起时，即'sendOffer'。代码位置技巧：记住new RTCPeerConnection()的时机就行
+//  也是：B接受了，A才把B的流渲染到自己的页面上。这时候B页面也应该出现A的流。可以借助A 监听B接收 时，发出Offer的动作里面 做B渲染A的流到自己页面。
 
 // 总结每个交互动作都需要：emit + on。通过信令服务器来沟通触发动作
 import { onMounted, ref } from 'vue'
@@ -28,12 +41,23 @@ import { io, type Socket } from 'socket.io-client'
 
 const localVideo = ref<HTMLVideoElement>()
 const remoteVideo = ref<HTMLVideoElement>()
+// 定义socket实例
+const socket = ref<Socket>()
+// 创建房间
+const roomId = '001'
+const peer = ref<any>()
+const localStream = ref<MediaStream>()
+// UI交互中间状态
+const calling = ref(false)
+const caller = ref(false) // 呼叫者标识
+const called = ref(false) // 被呼叫着标识
+const communicating = ref(false) // 通话中
 
 const getLocalStream = async () => {
   // 获取本地音视频流
   const stream = await navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: true
+    audio: true,
   })
   // 将流设置到 video 标签上播放
   localVideo.value!.srcObject = stream
@@ -43,13 +67,6 @@ const getLocalStream = async () => {
   return stream
 }
 
-// 定义socket实例
-const socket = ref<Socket>()
-// 创建房间
-const roomId = '001'
-const peer = ref<any>()
-const localStream = ref<MediaStream>()
-
 onMounted(async () => {
   // 2.尝试把本地视频流渲染到页面
   // getLocalStream()
@@ -57,12 +74,12 @@ onMounted(async () => {
   // 3.连接信令服务器
   const sock = io('localhost:3000') // 对应服务的端口
   sock.on('connectionSuccess', () => {
-    console.log('client connect success')
+    // console.log('client connect success')
     // 加入房间
     sock.emit('joinRoom', roomId)
 
     // B监听收到通话请求事件
-    sock.on('receiveCall', roomId => {
+    sock.on('receiveCall', (roomId) => {
       // 如果是B（被呼叫者）才做以下动作
       if (!caller.value) {
         calling.value = true
@@ -78,12 +95,31 @@ onMounted(async () => {
         peer.value = new RTCPeerConnection()
         // 添加本地音视频流
         peer.value.addStream(localStream.value)
+
+        // 获取candidate信息
+        peer.value.onicecandidate = (event: any) => {
+          if (event.candidate) {
+            // 向服务器发送candidate信息
+            sock.emit('sendCandidate', { roomId, candidate: event.candidate })
+          }
+        }
+        // 开始P2P连接
+        peer.value.onaddstream = (event: any) => {
+          // console.log(event, 'onaddstream-A页面拿到B的流')
+          // 更改UI
+          calling.value = false
+          communicating.value = true
+          // 拿到对方视频流
+          remoteVideo.value!.srcObject = event.stream
+          remoteVideo.value!.play()
+        }
+
         // 生成offer 开始交换SDP信息（媒体协商过程）
-        const offer = await peer.value.createOfffer({
+        const offer = await peer.value.createOffer({
           offerToReceiveAudio: 1,
-          offerToReceiveVideo: 1
+          offerToReceiveVideo: 1,
         })
-        console.log(offer, 'offer')
+        // console.log(offer, 'offer')
         // 设置本地描述的offer
         await peer.value.setLocalDescription(offer)
         // 通过信令服务器将offer信息发送给B
@@ -92,19 +128,38 @@ onMounted(async () => {
     })
 
     // B监听发送offer事件
-    sock.on('sendOffer', async offer => {
+    sock.on('sendOffer', async (offer) => {
       if (called.value) {
-        console.log('sendOffer', offer)
-        // 创建自己的RTCPeerConnection对象
-        peer.value = new RTCPeerConnection()
+        // console.log('sendOffer', offer)
         // B接收者创建自己的本地流并添加流
         const stream = await getLocalStream()
+        // 创建自己的RTCPeerConnection对象
+        peer.value = new RTCPeerConnection()
+        // 添加本地流
         peer.value.addStream(stream)
+
+        // 获取candidate信息
+        peer.value.onicecandidate = (event: any) => {
+          if (event.candidate) {
+            // 向服务器发送candidate信息
+            sock.emit('sendCandidate', { roomId, candidate: event.candidate })
+          }
+        }
+        // 开始P2P连接-添加对方流渲染到页面
+        peer.value.onaddstream = (event: any) => {
+          // 更改UI
+          calling.value = false
+          communicating.value = true
+          // 拿到对方视频流
+          remoteVideo.value!.srcObject = event.stream
+          remoteVideo.value!.play()
+        }
+
         // 设置远端描述的offer
-        peer.value.setRemoteDescription(offer)
+        await peer.value.setRemoteDescription(offer)
         // 生成answer
         const answer = await peer.value.createAnswer()
-        console.log(answer, 'answer')
+        // console.log(answer, 'answer')
         // 设置本地描述的answer
         await peer.value.setLocalDescription(answer)
         // 发送answer给信令服务器
@@ -113,23 +168,31 @@ onMounted(async () => {
     })
 
     // A监听收到answer事件
-    sock.on('receiveAnswer', async (answer: any) => {
+    sock.on('receiveAnswer', (answer: any) => {
       if (caller.value) {
         // 设置远端描述的answer
+        // console.log(answer, 'receiveAnswer')
         peer.value.setRemoteDescription(answer)
       }
+    })
+    // B监听发送candidate事件
+    sock.on('receiveCandidate', async (candidate: any) => {
+      // console.log('B收到candidate')
+      await peer.value.addIceCandidate(candidate)
+    })
+    // 两边监听挂断事件，重置状态
+    sock.on('hangUp', () => {
+      // console.log('hangUp')
+      reset()
     })
   })
   socket.value = sock
 })
 
-// UI交互中间状态
-const calling = ref(false)
-const caller = ref(false) // 呼叫者标识
-const called = ref(false) // 被呼叫着标识
-const communicating = ref(false) // 通话中
-
 const callRemote = async () => {
+  if (calling.value || communicating.value) {
+    return
+  }
   // ！发起视频前要先创建好房间
   caller.value = true
   calling.value = true
@@ -137,12 +200,25 @@ const callRemote = async () => {
   // 发给信令服务器告诉B 已经发起了请求，并告诉房间号信息
   socket.value?.emit('callRemote', roomId)
 }
-const hangUp = () => {
-  calling.value = false
-}
 const acceptCall = () => {
   // 接听
   socket.value?.emit('acceptCall', roomId)
+}
+const hangUp = () => {
+  // 重置很多状态-reset函数 两边都需要重置状态，所以需要发送给信令服务器去广播，页面监听挂断事件执行
+  // calling.value = false
+  socket.value?.emit('hangUp', roomId)
+}
+const reset = () => {
+  caller.value = false
+  called.value = false
+  calling.value = false
+  communicating.value = false
+  peer.value = null
+  localVideo.value!.srcObject = null
+  remoteVideo.value!.srcObject = null
+  localStream.value?.getTracks()[0].stop()
+  // localStream.value = undefined
 }
 </script>
 
@@ -159,29 +235,21 @@ const acceptCall = () => {
       </div>
       <!-- 发起时，接收者（被呼叫方）状态 什么时候触发 收到A的请求信息才显示-->
       <div v-if="called && calling" class="absolute top-2/3 left-32 flex flex-col items-center">
-        <p class="mb-4 text-white">收到视频邀请...</p>
+        <p v class="mb-4 text-white">收到视频邀请...</p>
         <div class="flex">
           <img @click="hangUp" src="/refuse.svg" class="w-16 cursor-pointer mr-4" alt="" />
-          <img
-            v-if="!communicating"
-            @click="acceptCall"
-            src="/accept.svg"
-            class="w-16 cursor-pointer"
-            alt="" />
+          <img @click="acceptCall" src="/accept.svg" class="w-16 cursor-pointer" alt="" />
         </div>
       </div>
     </div>
     <div class="flex gap-2 mb-4">
       <button
+        v-if="!calling && !communicating"
         class="rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white"
         @click="callRemote">
         发起视频
       </button>
-      <button
-        class="rounded-md bg-red-600 px-4 py-2 text-sm font-semibold text-white"
-        @click="hangUp">
-        挂断视频
-      </button>
+      <button class="rounded-md bg-red-600 px-4 py-2 text-sm font-semibold text-white" @click="hangUp">挂断视频</button>
     </div>
   </div>
 </template>
--- WebRTC-clone/webrtc-server/index.js
+++ WebRTC-clone/webrtc-server/index.js
@@ -3,39 +3,50 @@ const http = require('http')
 const server = http.createServer()
 
 const io = socket(server, {
-  cors: { origin: '*' }
+  cors: { origin: '*' },
 })
 
-io.on('connection', socket => {
+io.on('connection', (sock) => {
   console.log('server connect success')
   // 通知客户端
-  socket.emit('connectionSuccess')
+  sock.emit('connectionSuccess')
   // 监听房间创建信息
-  socket.on('joinRoom', roomId => {
-    console.log('joinRoom', roomId)
+  sock.on('joinRoom', (roomId) => {
     // 加入房间
-    socket.join(roomId)
+    sock.join(roomId)
   })
   // 监听发起通话请求
-  socket.on('callRemote', roomId => {
+  sock.on('callRemote', (roomId) => {
     // 触发一个收到请求事件-向这个房间中的所有人广播这个事件
     io.to(roomId).emit('receiveCall')
   })
   // 监听接受通话请求
-  socket.on('acceptCall', roomId => {
+  sock.on('acceptCall', (roomId) => {
     // 向这个房间中的所有人广播这个事件
     io.to(roomId).emit('acceptCall')
   })
   // 监听发起者的发送offer事件
-  socket.on('sendOffer', ({ offer, roomId }) => {
+  sock.on('sendOffer', ({ roomId, offer }) => {
     // 广播发送offer事件给接收者
     io.to(roomId).emit('sendOffer', offer)
   })
 
   // 监听接收者的发送answer事件
-  socket.on('sendAnswer', ({ answer, roomId }) => {
+  sock.on('sendAnswer', ({ roomId, answer }) => {
     // A要接收answer 广播receiveAnswer给发起者
     io.to(roomId).emit('receiveAnswer', answer)
+  })
+
+  // 监听A发送candidate事件
+  sock.on('sendCandidate', ({ roomId, candidate }) => {
+    // 广播sendCandidate给接收者
+    io.to(roomId).emit('receiveCandidate', candidate)
+  })
+
+  // 监听拒绝/挂断事件
+  sock.on('hangUp', (roomId) => {
+    // 广播hangUp给接收者
+    io.to(roomId).emit('hangUp')
   })
 })
 
